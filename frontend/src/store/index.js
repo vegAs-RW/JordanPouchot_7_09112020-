674cca4e35548934e788c5bdddade417114d4c6e
vegAs-RW <jordan.pouchot2@gmail.com>
2021-12-01 11:50:21 +0100

    modif du store vue

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,71 @@
 import { createStore } from 'vuex'
 
+const axios = require ('axios');
+
+const instance = axios.create({
+    baseURL: 'http://localhost:4000/api/users/'
+  });
+
+  
+
 export default createStore({
     state: {
-        userId:null,
-        token:null
+        status: '',
+        user: {
+            isLogged: false,
+            userId : '-1',
+            token: '',
+        }
     },
     mutations: {
+        setStatus: function (state, status) {
+            state.status = status;
+        },
+        logUser: function (state, user) {
+            instance.defaults.headers.common['Authorization'] = user.token;
+            state.user = user;
+        },
+        logout: function (state) {
+            state.user = {
+                userId: -1,
+                token:"",
+            }
+
+        }
     },
     actions: {
+        login: ({ commit }, userInfos) => {
+            commit ('setStatus', 'loading');
+            return new Promise ((resolve, reject) => {
+            instance.post('/login', userInfos)
+              .then(function (response) {
+                commit ('setStatus', '');
+                commit ('logUser', response.data);
+                resolve(response);
+              })
+              .catch(function (error) {
+                commit ('setStatus', 'error_login');
+                reject(error);
+              });
+            })
+        },
+        register: ({ commit }, userInfos) => {
+            return new Promise ((resolve, reject) => {
+                commit;
+            instance.post('/register', userInfos)
+              .then(function (response) {
+                commit ('setStatus', 'created');
+                resolve(response);
+              })
+              .catch(function (error) {
+                commit ('setStatus', 'error_create');
+                reject(error);
+              }); 
+            })
+        },
+        
     },
     getters: {
-        isLogged:state => state.userId !== null && state.token !== null
+        
     }
 })
