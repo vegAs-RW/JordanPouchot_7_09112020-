08934602dbf93aeb9fd54c89c11b35a4fb0ba4f3
vegAs-RW <jordan.pouchot2@gmail.com>
2021-12-01 11:45:36 +0100

    modif des routes user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+/*module.exports = app => {
+    const users = require("../controllers/user.controller");
+    const auth = require('../middleware/auth');
 
-const userCtrl = require('../controllers/user.controller')
+    var router = require("express").Router();
+  
+    // Create a new Tutorial
+    router.post("/register", users.signup);
+  
+    // Retrieve all Tutorials
+    router.post("/login", users.login);
 
-router.post('/auth/signup', userCtrl.signup);
-router.post('/auth/login', userCtrl.login);
-router.delete('/accounts/:id', auth, userCtrl.deleteUser);
+    // Delete a Tutorial with id
+    router.delete("/:id", auth, users.deleteUser);
+  
+  
+    app.use('/api/users', router);
+  };*/
 
-module.exports = router;
+  const express = require('express');
+  const router = express.Router();
+  const auth = require('../middleware/auth');
+  const userCtrl = require('../controllers/user.controller');
+  
+  
+  router.post('/register', userCtrl.signup);
+  router.post('/login', userCtrl.login);
+  router.delete('/', auth, userCtrl.delete);
+  
+  module.exports = router;
