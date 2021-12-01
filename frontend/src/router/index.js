bd2444d7242bca40d4c10502897eb8d085d556bd
vegAs-RW <jordan.pouchot2@gmail.com>
2021-12-01 11:49:46 +0100

    modif du router vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
-
+import Home from '../views/Home.vue'
+import Profil from '../views/Profil.vue'
 const routes = [
   {
     path: '/',
     name: 'Login',
     component: Login
   },
-  
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  /*{
+    path: '/profil',
+    name: 'Profil',
+    component: Profil
+  },*/
+  {
+    path: '/profil/:userId',
+    name: 'UserProfil',
+    component: Profil
+  },
 ]
 
 const router = createRouter({
