3223b81e29f61270ba253d9570cc8742f2eca94b
vegAs-RW <jordan.pouchot2@gmail.com>
2021-12-01 11:44:58 +0100

    modif multer

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,16 +1,21 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif'
+};
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (file.fieldname === "post_image") cb (null, "./images/posts/");
-    else if (file.fieldname === "profil_image") cb (null, "./images/profils/");
+  destination: (req, file, callback) => {
+    callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    callback(null, Date.now() + path.extname(file.originalname));
-  },
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
 });
 
-const upload = multer({storage: storage});
-
-module.exports = upload
+module.exports = multer({storage: storage}).single('image');
