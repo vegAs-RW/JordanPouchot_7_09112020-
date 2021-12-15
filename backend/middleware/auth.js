acf9af677eba8c40cd3364a41a920bb119dec763
vegAs-RW <jordan.pouchot2@gmail.com>
2021-12-15 09:10:03 +0100

    modif du middleware auth + modif de la config multer

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,18 @@
+// Permet d'importer jsonwebtoken
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
 
+// Permet de vérifier le token envoyé par le frontend
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      next();
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'JWT_SECRET_TOKEN');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable';
+        } else {            
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({ error: 'Requête non authentifiée'});
     }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
-  }
 };
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,21 +1,26 @@
+// Permet d'importer multer
 const multer = require('multer');
 
+// Permet de définir l'extension des fichiers images
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png',
-  'image/gif': 'gif'
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
 };
 
+// Permet de configurer multer
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+    // Permet de stocker les images dans le dossier images
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    // Permet de générer un nouveau nom de fichier image
+    filename: (req, file, callback) => {
+        const name = file.originalname.split('.')[0].split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
 });
 
-module.exports = multer({storage: storage}).single('image');
+// Permet d'exporter le middleware multer
+module.exports = multer({storage}).single('image');
